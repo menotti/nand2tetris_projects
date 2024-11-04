e25dcdd23136ab99392e800f43a357580d0bd707
Jnt-Dev <jonatareg@gmail.com>
2024-11-04 18:09:44 -0300

    corrections RAM 64

--- 03/a/RAM64.v
+++ 03/a/RAM64.v
@@ -1,19 +1,20 @@
 module RAM64(
+    input clk, load
     input [15:0] in,
     input [5:0] address,
-    input load, clk,
     output reg [15:0] out);
 
-    reg [15:0] memory [63:0];  // 64 registradores, cada um com 16 bits de largura
+    wire [15:0] r1_o, r2_o, r3_o, r4_o, r5_o, r6_o, r7_o, r8_o
 
-    always @(posedge clk) begin
-        if (load) begin
-            memory[address] <= in;
-        end
-    end
+    RAM8 r1(clk, load, address[2:0], r1_o);
+    RAM8 r2(clk, load, address[2:0], r2_o);
+    RAM8 r3(clk, load, address[2:0], r3_o);
+    RAM8 r4(clk, load, address[2:0], r4_o);
+    RAM8 r5(clk, load, address[2:0], r5_o);
+    RAM8 r6(clk, load, address[2:0], r6_o);
+    RAM8 r7(clk, load, address[2:0], r7_o);
+    RAM8 r8(clk, load, address[2:0], r8_o);
 
-    always @(negedge clk) begin
-        out = memory[address];
-    end
+    Mux8Way16 mux1(r1_o, r2_o, r3_o, r4_o, r5_o, r6_o, r7_o, r8_o, address[5:3], out);
 
 endmodule
