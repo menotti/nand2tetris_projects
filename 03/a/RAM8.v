2165160c49742c62cbe7b69460ca661e394e5b01
kioshiotani <brunokioshiotani@gmail.com>
2024-11-04 23:06:23 -0300

    Modified: RAM8.v, RAM8_tb.v

--- 03/a/RAM8.v
+++ 03/a/RAM8.v
@@ -4,15 +4,18 @@ module RAM8(input clk, load,
 	    output [15:0] out);
     
     wire [15:0] r1_o, r2_o, r3_o, r4_o, r5_o, r6_o, r7_o, r8_o; 
+    wire load1, load2, load3, load4, load5, load6, load7, load8;
 
-    Register r1 (clk, load, in, r1_o);
-    Register r2 (clk, load, in, r2_o);
-    Register r3 (clk, load, in, r3_o);
-    Register r4 (clk, load, in, r4_o);
-    Register r5 (clk, load, in, r5_o);
-    Register r6 (clk, load, in, r6_o);
-    Register r7 (clk, load, in, r7_o);
-    Register r8 (clk, load, in, r8_o);
+    DMux8Way dmx (load, address, load1, load2, load3, load4, load5, load6, load7, load8);
+
+    Register r1 (clk, load1, in, r1_o);
+    Register r2 (clk, load2, in, r2_o);
+    Register r3 (clk, load3, in, r3_o);
+    Register r4 (clk, load4, in, r4_o);
+    Register r5 (clk, load5, in, r5_o);
+    Register r6 (clk, load6, in, r6_o);
+    Register r7 (clk, load7, in, r7_o);
+    Register r8 (clk, load8, in, r8_o);
 
     Mux8Way16 mx (r1_o, r2_o, r3_o, r4_o, r5_o, r6_o, r7_o, r8_o, address, out);
 
--- 03/a/RAM8_tb.v
+++ 03/a/RAM8_tb.v
@@ -1,50 +1,64 @@
 `timescale 1ns / 1ns
 `default_nettype none
+
 module RAM8_tb();
     
     integer file;
-    integer i;
+    integer i = 0;
 
     reg clk;
     reg signed [15:0] in;
     reg load;
     reg [2:0] address;
     wire signed [15:0] out;
-    reg reset;
 
-
-    reg [79:0] testvectors [0:171];
+    reg [79:0] testvectors [171:0];
 
     RAM8 dut(
-	    .clk(),
-	    .load(load),
-	    .in(in),
-	    .address(address),
-	    .out(out)
+        .clk(clk),
+        .load(load),
+        .in(in),
+        .address(address),
+        .out(out)
     );
 
-    task display;
-        $fwrite(file, "%04h_%04h_%04h_%04h_%04h_\n", clk, in, load, address, out);
+    task apply;
+	in    = testvectors[i][63:48];
+        load  = testvectors[i][32];
+        address = testvectors[i][18:16];
+    endtask
+
+    task write;
+	$fwrite(file, "%04h_%04h_%04h_%04h\n", in, load, address, out);
+    endtask
+
+    task tick;
+	    clk = 0;
+    endtask
+
+    task tock;
+	    clk = 1;
     endtask
 
     initial begin
         $dumpfile("RAM8_tb.vcd");
         $dumpvars(0, RAM8_tb);
-        file = $fopen("RAM8.out","w");
+        file = $fopen("RAM8.out", "w");
         $readmemh("RAM8.tv", testvectors);
 
-	reset = 0; #1 reset = 1; #1 reset = 0;
-
-	for(i = 0; i < 172; i = i + 1) begin
-		clk = testvectors[i][64];
-		in    = testvectors[i][63:48];
-		load  = testvectors[i][32];
-		address = testvectors[i][18:16];
-		display();
-	end
     end
+
+
+    always begin
+	if (i == 172) $finish;
+	apply();
+	#100
+	tick();
+	#100
+	write();
+	tick();
+	i = i + 1;
+    end
+
 endmodule
-
-
-
 
