02ab568e394359ac88d641d3a83a1267b98ac0cd
Cauã Borges Faria <cauaborges@estudante.ufscar.br>
2024-11-05 23:46:22 -0300

    refactor RAM512_tb
    Still working on resolving the clock issue, but had some progress.

--- 03/b/RAM512_tb.v
+++ 03/b/RAM512_tb.v
@@ -3,34 +3,32 @@
 
 module RAM512_tb();
 
+    integer i;
     integer file;
     string stime, tick_tock;
 
-    reg clk = 1;
     reg load;
     reg signed [15:0] in;
     reg [8:0] address;
     wire signed [15:0] out;
     reg [79:0] testvectors [318:0];
-    reg [31:0] vectornum = 0;
+    reg clock;
 
     RAM512 dut (
         .in(in),
         .address(address),
         .load(load),
-        .clk(clk),
+        .clk(clock),
         .out(out)
     );
 
+    reg [15:0] ttime;
     task display;
-        
-        in       = testvectors[vectornum][79:64];
-        load     = testvectors[vectornum][48];
-        address  = testvectors[vectornum][47:39];
-        vectornum = vectornum + 1;
-        
-        // Formata e escreve a linha de saída
-        $sformat(stime, "%0d%s", vectornum>>1, tick_tock);
+        ttime     = testvectors[i][79:64];
+        in       = testvectors[i][63:48];
+        load     = testvectors[i][32];
+        address  = testvectors[i][31:16];
+        $sformat(stime, "%0d%s", i, tick_tock);
         $fwrite(file, "| %-4s | %6d | %1b | %3d | %6d |\n", stime, in, load, address, out);
     endtask
 
@@ -39,21 +37,29 @@ module RAM512_tb();
         $dumpvars(0, RAM512_tb);
         file = $fopen("RAM512.out", "w");
         $fwrite(file, "| time |   in   |load |address|  out   |\n");
-        $readmemh("RAM512.tv", testvectors);
-        forever #0.5 clk = !clk;
-    end
 
-    always @(posedge clk) begin
-        tick_tock = " ";
-        display();
-        if (testvectors[vectornum][0] === 1'bx) begin
-            $finish;
+        $readmemh("RAM512.tv", testvectors, 0, 318);
+
+        for (i = 0; i < 319; i = i + 1) begin
+            ttime = testvectors[i][79:64];
+            case(ttime)
+                16'b0000000000000000: begin
+                    clock = 0;
+                    tick_tock = " ";
+                end
+                16'b0000000000000001: begin
+                    clock = 1;
+                    tick_tock = "+";
+                end
+            endcase
+
+            display();
+            #1;
+
+            if (testvectors[i][0] === 1'bx) begin
+                $finish;
+            end
         end
-    end
-
-    always @(negedge clk) begin
-        tick_tock = "+";
-        display();
     end
 
 endmodule
