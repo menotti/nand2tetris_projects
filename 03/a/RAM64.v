950baef6850abac54ea5ac9a33cf295591de951b
Bruno Otani <brunokioshiotani@gmail.com>
2024-11-10 15:59:07 -0300

    Fixed RAM64.v, Fixed RAM64_tb.v, Fixed RAM64.tv

--- 03/a/RAM64.v
+++ 03/a/RAM64.v
@@ -6,15 +6,18 @@ module RAM64(
 );
 
     wire [15:0] r1_o, r2_o, r3_o, r4_o, r5_o, r6_o, r7_o, r8_o;
+    wire load1, load2, load3, load4, load5, load6, load7, load8;
 
-    RAM8 r1(clk, load, in, address[2:0], r1_o);
-    RAM8 r2(clk, load, in, address[2:0], r2_o);
-    RAM8 r3(clk, load, in, address[2:0], r3_o);
-    RAM8 r4(clk, load, in, address[2:0], r4_o);
-    RAM8 r5(clk, load, in, address[2:0], r5_o);
-    RAM8 r6(clk, load, in, address[2:0], r6_o);
-    RAM8 r7(clk, load, in, address[2:0], r7_o);
-    RAM8 r8(clk, load, in, address[2:0], r8_o);
+    DMux8Way dmx (load, address[5:3], load1, load2, load3, load4, load5, load6, load7, load8);
+
+    RAM8 r1(clk, load1, in, address[2:0], r1_o);
+    RAM8 r2(clk, load2, in, address[2:0], r2_o);
+    RAM8 r3(clk, load3, in, address[2:0], r3_o);
+    RAM8 r4(clk, load4, in, address[2:0], r4_o);
+    RAM8 r5(clk, load5, in, address[2:0], r5_o);
+    RAM8 r6(clk, load6, in, address[2:0], r6_o);
+    RAM8 r7(clk, load7, in, address[2:0], r7_o);
+    RAM8 r8(clk, load8, in, address[2:0], r8_o);
 
     Mux8Way16 mux1(r1_o, r2_o, r3_o, r4_o, r5_o, r6_o, r7_o, r8_o, address[5:3], out);
 
--- 03/a/RAM64_tb.v
+++ 03/a/RAM64_tb.v
@@ -2,53 +2,83 @@
 `default_nettype none
 
 module RAM64_tb();
-
+    
     integer file;
-    string stime, tick_tock;
+    integer i = 0;
+    integer time_count;
+    integer clk_cycle;     
+    integer clk_level;
 
+    string stime;
+    string tick_tock;
+
+    reg clk = 0;
+    reg clk_prev;
     reg signed [15:0] in;
-    reg address [5:0] address;
-    reg load, clk = 1;
+    reg load;
+    reg [5:0] address;
     wire signed [15:0] out;
-    reg [63:0] testvectors [318:0];
-    reg [31:0] vectornum = 0;
+
+    reg [79:0] testvectors [318:0];
 
     RAM64 dut(
+        .clk(clk),
+        .load(load),
         .in(in),
         .address(address),
-        .load(load),
-        .clk(clk),
         .out(out)
     );
 
-    task display;
-        in =      testvectors[vectornum][63:48];
-        load =    testvectors[vectornum][32];
-        address = testvectors[vectornum][31:16];
-        vectornum = vectornum + 1;
-        $sformat(stime, "%0d%s", vectornum>>1, tick_tock);
-        $fwrite(file, "| %-4s |  %6d   | %1b |  %2d  |  %6d   |\n", stime, in, load, address, out);
+    task apply;
+	clk     = testvectors[i][64];
+	in      = testvectors[i][63:48];
+        load    = testvectors[i][32];
+        address = testvectors[i][21:16];
+    endtask
+
+    task write;
+	$sformat(stime, "%0d%s", time_count, tick_tock);
+	$fwrite(file, "| %-4s | %6d |  %1b  |  %3d  | %6d |\n", stime, in, load, address, out);
     endtask
 
     initial begin
-        $dumpfile("RAM64.vcd");
-        $dumpvars(0, RAM64);
-        file = $fopen("RAM64.out","w");
-        $fwrite(file, "| time |   in   |load |address|  out   |\n");
-        $readmemh("RAM64.tv", testvectors);
-        forever #0.5 clk = !clk;
-    end
+        $dumpfile("RAM64_tb.vcd");
+        $dumpvars(0, RAM64_tb);
+        file = $fopen("RAM64.out", "w");
+	$fwrite(file, "| time |   in   |load |address|  out   |\n");
+        $readmemh("RAM64.tv", testvectors, 0, 318);
 
-    always @(posedge clk) begin
-        tick_tock = " ";
-        display();
-        if (testvectors[vectornum][0] === 1'bx)
-            $finish;
-    end
 
-    always @(negedge clk) begin
-        tick_tock = "+";
-        display();
-    end
+	i = 0;
+	apply();
+	time_count = 0;
+	tick_tock = "+";
+	clk_level = 1;
+	clk_cycle = 1;
+	write();
+	clk_prev = clk;
 
+	for (i = 1; i < 319; i = i + 1) begin
+		apply();
+		if (clk_prev !== clk) begin 
+			clk_cycle = clk_cycle + 1;
+			if (clk_level == 1) begin
+				tick_tock = " ";
+				clk_level = 0;
+			end
+			else begin
+				tick_tock = "+";
+				clk_level = 1;
+			end
+		end
+		if (clk_cycle == 2) begin
+			clk_cycle = 0;
+			time_count = time_count + 1;
+		end
+		clk_prev = clk;
+		#1;
+		write();
+	end
+    end
 endmodule
+
