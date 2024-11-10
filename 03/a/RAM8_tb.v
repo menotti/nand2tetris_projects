356ab698876ab82e3a2d239cef1fabbef6479aff
Bruno Otani <brunokioshiotani@gmail.com>
2024-11-10 15:12:44 -0300

    Modified RAM8_tb.v
    Change variables names

--- 03/a/RAM8_tb.v
+++ 03/a/RAM8_tb.v
@@ -5,19 +5,19 @@ module RAM8_tb();
     
     integer file;
     integer i = 0;
-    integer times2 = 0;
     integer time_count;
-    integer titock;
-    integer level;
+    integer clk_cycle;     
+    integer clk_level;
+
     string stime;
     string tick_tock;
 
     reg clk = 0;
+    reg clk_prev;
     reg signed [15:0] in;
     reg load;
     reg [2:0] address;
     wire signed [15:0] out;
-    reg clk2;
 
     reg [79:0] testvectors [171:0];
 
@@ -53,32 +53,31 @@ module RAM8_tb();
 	apply();
 	time_count = 0;
 	tick_tock = "+";
-	level = 1;
-	titock = 1;
+	clk_level = 1;
+	clk_cycle = 1;
 	write();
-	clk2 = clk;
+	clk_prev = clk;
 
 	for (i = 1; i < 172; i = i + 1) begin
 		apply();
-		if (clk2 !== clk) begin 
-			titock = titock + 1;
-			if (level == 1) begin
+		if (clk_prev !== clk) begin 
+			clk_cycle = clk_cycle + 1;
+			if (clk_level == 1) begin
 				tick_tock = " ";
-				level = 0;
+				clk_level = 0;
 			end
 			else begin
 				tick_tock = "+";
-				level = 1;
+				clk_level = 1;
 			end
 		end
-		if (titock == 2) begin
-			titock = 0;
+		if (clk_cycle == 2) begin
+			clk_cycle = 0;
 			time_count = time_count + 1;
 		end
-		clk2 = clk;
+		clk_prev = clk;
 		#1;
 		write();
-		#1;
 	end
     end
 endmodule
