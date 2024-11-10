5106949a1fe7589ce8794ef252cc2ecf3a3fb43a
Bruno Otani <brunokioshiotani@gmail.com>
2024-11-10 16:36:43 -0300

    Fixed RAM512_tb.v

--- 03/b/RAM512_tb.v
+++ 03/b/RAM512_tb.v
@@ -2,64 +2,82 @@
 `default_nettype none
 
 module RAM512_tb();
-
-    integer i;
+    
     integer file;
-    string stime, tick_tock;
+    integer i = 0;
+    integer time_count;
+    integer clk_cycle;     
+    integer clk_level;
 
-    reg load;
+    string stime;
+    string tick_tock;
+
+    reg clk = 0;
+    reg clk_prev;
     reg signed [15:0] in;
+    reg load;
     reg [8:0] address;
     wire signed [15:0] out;
-    reg [79:0] testvectors [318:0];
-    reg clock;
 
-    RAM512 dut (
+    reg [79:0] testvectors [318:0];
+
+    RAM512 dut(
+        .clk(clk),
+        .load(load),
         .in(in),
         .address(address),
-        .load(load),
-        .clk(clock),
         .out(out)
     );
 
-    reg [15:0] ttime;
-    task display;
-        ttime     = testvectors[i][79:64];
-        in       = testvectors[i][63:48];
-        load     = testvectors[i][32];
-        address  = testvectors[i][31:16];
-        $sformat(stime, "%0d%s", i, tick_tock);
-        $fwrite(file, "| %-4s | %6d | %1b | %3d | %6d |\n", stime, in, load, address, out);
+    task apply;
+	clk     = testvectors[i][64];
+	in      = testvectors[i][63:48];
+        load    = testvectors[i][32];
+        address = testvectors[i][24:16];
+    endtask
+
+    task write;
+	$sformat(stime, "%0d%s", time_count, tick_tock);
+	$fwrite(file, "| %-4s | %6d |  %1b  |  %3d  | %6d |\n", stime, in, load, address, out);
     endtask
 
     initial begin
         $dumpfile("RAM512_tb.vcd");
         $dumpvars(0, RAM512_tb);
         file = $fopen("RAM512.out", "w");
-        $fwrite(file, "| time |   in   |load |address|  out   |\n");
-
+	$fwrite(file, "| time |   in   |load |address|  out   |\n");
         $readmemh("RAM512.tv", testvectors, 0, 318);
 
-        for (i = 0; i < 319; i = i + 1) begin
-            ttime = testvectors[i][79:64];
-            case(ttime)
-                16'b0000000000000000: begin
-                    clock = 0;
-                    tick_tock = " ";
-                end
-                16'b0000000000000001: begin
-                    clock = 1;
-                    tick_tock = "+";
-                end
-            endcase
+	i = 0;
+	apply();
+	time_count = 0;
+	tick_tock = "+";
+	clk_level = 1;
+	clk_cycle = 1;
+	write();
+	clk_prev = clk;
 
-            display();
-            #1;
-
-            if (testvectors[i][0] === 1'bx) begin
-                $finish;
-            end
-        end
+	for (i = 1; i < 319; i = i + 1) begin
+		apply();
+		if (clk_prev !== clk) begin 
+			clk_cycle = clk_cycle + 1;
+			if (clk_level == 1) begin
+				tick_tock = " ";
+				clk_level = 0;
+			end
+			else begin
+				tick_tock = "+";
+				clk_level = 1;
+			end
+		end
+		if (clk_cycle == 2) begin
+			clk_cycle = 0;
+			time_count = time_count + 1;
+		end
+		clk_prev = clk;
+		#1;
+		write();
+	end
     end
-
 endmodule
+
